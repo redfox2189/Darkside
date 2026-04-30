81b8be28dc5e9557ba19b6d8171d73be0854ef32
Miles <miles.christiansen@southeasttech.edu>
2026-04-30 05:14:48 -0500

    Done

--- Darkside/src/App.tsx
+++ Darkside/src/App.tsx
@@ -1,121 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [isDark, setIsDark] = useState(true)
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <body style={{ backgroundColor: isDark ? "rgb(110, 16, 180)" : "rgb(20, 236, 146)"}}>
+    <section style={{
+      backgroundColor: isDark ? "rgb(100, 16, 180)" : "rgb(50, 245, 164)", height: "100vh"
+    }} id="center">
+      <div>
+        <h2 style={{ color: isDark ? "white" : "black" }}>Times "The Button" has been pushed {count}</h2>
+        <br />
+        <br />
+      </div>
 
-      <div className="ticks"></div>
+      <button
+        type="button"
+        className="counter"
+        style={{ background: isDark ? "rgba(170, 59, 255, 0.1)" : "rgba(202, 243, 226, 0.2)", color: isDark ? "#aa3bff" : "#176939" }}
+        onClick={() =>{setCount(count + 1) 
+          if((count+1)%10 === 0 )alert("Congrats you clicked "+ (count+1) + " times")
+        }}
+      >
+        The Button
+      </button>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      <button
+        type="button"
+        className="counter"
+        style={{ background: isDark ? "rgba(170, 59, 255, 0.1)" : "rgba(202, 243, 226, 0.2)", color: isDark ? "#aa3bff" : "#176939" }}
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+        onClick={() =>
+          setIsDark(prev => !prev)
+        }
+      >
+        Change Background
+      </button>
+    </section>
+    </body>
   )
 }
 
